3672c00d0bc2cf206877b2780db2318a2e5387fd
helenaLahora <127090172+helenaLahora@users.noreply.github.com>
2023-04-11 22:34:57 +0200

    el jugador ja no cau al infinit + camara arreglada

--- PEV_P02/Assets/Scripts/Player.cs
+++ PEV_P02/Assets/Scripts/Player.cs
@@ -16,29 +16,33 @@ public class Player : MonoBehaviour {
 	// Gravedad aplicada al jugador
 	public float gravity = 20.0f;
 
+	// Fuerza de salto
+	public float jumpForce = 8.0f;
+
 	void Start () {
 		// Se obtienen los componentes Animator y CharacterController del objeto al que se le asigna el script
-		controller = GetComponent <CharacterController>();
+		controller = GetComponent<CharacterController>();
 		anim = gameObject.GetComponentInChildren<Animator>();
 
-		// Se define la capa "ground" y se indica que la colisión está habilitada
-		// Para ello se obtiene su número identificador con LayerMask.NameToLayer() y se ignora la colisión en ambas direcciones con Physics.IgnoreLayerCollision()
-		int groundLayer = LayerMask.NameToLayer("ground");
-		Physics.IgnoreLayerCollision(gameObject.layer, groundLayer, false);
 	}
 
 	void Update (){
-		if (Input.GetKey ("w")) {
+		if (Input.GetKey("w")) {
 			// Si se pulsa la tecla "w", se activa la animación de correr
-			anim.SetInteger ("AnimationPar", 1);
-		}  else {
+			anim.SetInteger("AnimationPar", 1);
+		} else {
 			// De lo contrario, se desactiva la animación
-			anim.SetInteger ("AnimationPar", 0);
+			anim.SetInteger("AnimationPar", 0);
 		}
 
-		if(controller.isGrounded){
+		if (controller.isGrounded) {
 			// Si el jugador está en el suelo, se calcula su dirección de movimiento en base a la entrada del jugador
 			moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				// Si se pulsa la tecla espacio, se aplica una fuerza de salto a la dirección vertical de movimiento del jugador
+				moveDirection.y = jumpForce;
+			}
 		}
 
 		// Se calcula la dirección de rotación en base a la entrada del jugador
--- PEV_P02/Assets/Scripts/ThirdPersonCamera.cs
+++ PEV_P02/Assets/Scripts/ThirdPersonCamera.cs
@@ -1,36 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCamera : MonoBehaviour // declaramos la clase
+public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform target; // objeto a seguir
-    public float distance = 5.0f; // distancia inicial de la cámara
-    public float height = 2.0f; // altura de la cámara sobre el objeto
-    public float damping = 5.0f; // suavidad de movimiento de la cámara
-    public float zoomSpeed = 2.0f; // velocidad de zoom
-    public float minDistance = 1.0f; // distancia mínima permitida
-    public float maxDistance = 10.0f; // distancia máxima permitida
+    // Establece los ángulos mínimo y máximo de la cámara
+    public float Y_ANGLE_MIN = 0.0f;
+    public float Y_ANGLE_MAX = 50.0f;
 
-    private float currentDistance; // distancia actual
-    private float desiredDistance; // distancia deseada
-    private Vector3 offset; // offset (distancia) entre la cámara y el objeto
+    // El objeto que la cámara seguirá
+    public Transform lookAt;
+    // La distancia mínima y máxima entre la cámara y el objeto a seguir
+    public float minDistance = 3.0f;
+    public float maxDistance = 10.0f;
+    // La distancia actual entre la cámara y el objeto a seguir
+    private float currentDistance = 5.0f;
 
-    void Start()
+    // El ángulo actual de la cámara en el eje X
+    private float currentX = 0.0f;
+    // El ángulo actual de la cámara en el eje Y
+    private float currentY = 45.0f;
+    // La sensibilidad de la cámara en el eje X
+    private float sensitivityX = 20.0f;
+    // La sensibilidad de la cámara en el eje Y
+    private float sensitivityY = 20.0f;
+    // La transformación de la cámara
+    private Transform camTransform;
+
+    private void Start()
     {
-        offset = transform.position - target.position; // calculamos el offset
-        currentDistance = distance; // establecemos la distancia actual como la distancia inicial
-        desiredDistance = distance; // establecemos la distancia deseada como la distancia inicial
+        // Asigna la transformación de la cámara a la transformación del objeto
+        camTransform = transform;
     }
 
-    void LateUpdate()
+    private void Update()
     {
-        float horizontal = Input.GetAxis("Mouse X") * damping; // obtenemos el movimiento horizontal del mouse
-        float vertical = Input.GetAxis("Mouse Y") * damping; // obtenemos el movimiento vertical del mouse
+        // Obtiene la entrada del ratón y la multiplica por la sensibilidad en los ejes X e Y
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY += Input.GetAxis("Mouse Y") * sensitivityY;
 
-        Quaternion rotation = Quaternion.Euler(vertical, horizontal, 0); // calculamos la rotación de la cámara
-        Vector3 desiredPosition = target.position + (rotation * offset * currentDistance); // calculamos la posición deseada de la cámara
+        // Limita el ángulo de la cámara en el eje Y para que no se pueda mirar hacia abajo o hacia arriba demasiado lejos
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
 
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping); // movemos la cámara hacia la posición deseada utilizando la función Lerp
+        // Controla el zoom mediante la rueda del ratón
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        currentDistance -= scroll * 5;
+        currentDistance = Mathf.Clamp(currentDistance, minDistance, maxDistance);
+    }
 
-        currentDistance = Mathf.Clamp(currentDistance - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, minDistance, maxDistance); // actualizamos la distancia actual utilizando el valor del mouse scrollwheel y asegurándonos de que esté dentro de los límites permitidos
+    private void LateUpdate()
+    {
+        // Crea un vector de dirección apuntando hacia atrás desde la posición de la cámara
+        Vector3 dir = new Vector3(0, 0, -currentDistance);
+        // Crea una rotación utilizando los ángulos actuales de la cámara
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        // Establece la posición de la cámara utilizando la posición del objeto a seguir, la rotación y el vector de dirección
+        camTransform.position = lookAt.position + rotation * dir;
+        // Hace que la cámara apunte hacia el objeto a seguir
+        camTransform.LookAt(lookAt.position);
     }
 }
