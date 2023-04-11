00d50f2b9972444a0a81914b57796078b2a1d5d7
helenaLahora <127090172+helenaLahora@users.noreply.github.com>
2023-04-11 22:56:49 +0200

    minimapa i no sé que més he arreglat

---
+++ PEV_P02/Assets/Scripts/Player/MiniMapa.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapa : MonoBehaviour
+{
+
+    public Transform Player;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = Player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
