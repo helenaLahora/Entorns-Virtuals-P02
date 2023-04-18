fe090b717b5dfb22f48765e49631199a44a02e49
yunsg02 <127090156+yunsg02@users.noreply.github.com>
2023-04-18 10:56:47 +0200

    ChangeColor

---
+++ PEV_P02/Assets/Scripts/Sistema Salut/IChangeColor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IChangeColor
+{
+    //Dos mètodes
+    void SetColor(Color color);//li dic el color que s'ha de ficar
+    Color GetColor(); //Mètode que ens torna el color que té en aquell moment
+    
+}
