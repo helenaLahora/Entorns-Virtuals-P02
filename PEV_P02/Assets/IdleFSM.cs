a319a8562c3640865f03379a63cf85e74128a8f1
yunsg02 <127090156+yunsg02@users.noreply.github.com>
2023-04-28 11:01:53 +0200

    waypointss

---
+++ PEV_P02/Assets/IdleFSM.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleFSM : StateMachineBehaviour
+{
+    Transform _player;
+    float _timer;
+
+    public float detectionDistance = 4;
+    public float WaitTime = 2;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform; //dins de state enter sempre
+        _timer = 0;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Execute();
+        CheckTriggers(animator);
+    }
+
+    private void Execute()
+    {
+        _timer += Time.deltaTime; //augmentem el temps continuament
+    }
+
+    private void CheckTriggers(Animator animator)
+    {
+        bool isPlayerClose = IsPlayerClose(_player, animator.transform);
+        animator.SetBool("IsChasing", isPlayerClose); //canvia d'animaci? segons si el personatge est? a prop dels enemics
+
+        bool timeUp = IsTimeUp();
+        animator.SetBool("IsPatrolling", timeUp); //canvia d'animaci? segons el wait time
+    }
+
+    private bool IsTimeUp()
+    {
+        return _timer > WaitTime; //ho retorna si ?s m?s gran que el wait time
+    }
+
+    private bool IsPlayerClose(Transform player, Transform mySelf)
+    {
+        return Vector3.Distance(player.position, mySelf.position) < detectionDistance; //per saber si el player esta a prop de l'enemic
+    }
+}
