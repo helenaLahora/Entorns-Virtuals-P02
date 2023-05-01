e89c795689a8ad42ee140d59edf49ade64f2c100
yunsg02 <127090156+yunsg02@users.noreply.github.com>
2023-05-01 18:38:53 +0200

    Orden

--- PEV_P02/Assets/Scripts/AnnaScripts/ITakeDamage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ITakeDamage 
-{
-    //per rebre mal s'ha de tenir un mètode que expliqui perquè fa mal
-    void TakeDamage(float amount);
-}
